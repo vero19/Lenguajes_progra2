260b82c3640945333bb7ab6f406e9b028beb9f66
Josuepali <jmasisal@gmail.com>
2013-10-09 22:40:45 -0600

    le agregue un nuevo espacio al nodo para el argumento de la regla

--- NodosListaDoble.java
+++ NodosListaDoble.java
@@ -5,20 +5,23 @@ class NodosListaDoble {
  // datos amigables para que la Clase Lista Tenga un acceso directo
      Boolean check;
      String regla;
+     String argumento;
      NodosListaDoble siguiente;
      NodosListaDoble anterior;
 
 //Construtor 
- NodosListaDoble (String reg)
+ NodosListaDoble (String reg, String arg)
   {  regla = reg;
+  	 argumento = arg;
   	 check = false;
      siguiente = null; 
      anterior = null;
   }
 
 // Constructor Crea un nodo del Tipo Object y al siguiente nodo de la lista
-NodosListaDoble (String reg, NodosListaDoble signodo)
+NodosListaDoble (String reg, String arg, NodosListaDoble signodo)
 {   regla = reg;
+	argumento = arg;
   	check = false;
     siguiente = signodo; //siguiente se refiere al siguiente nodo
 }
